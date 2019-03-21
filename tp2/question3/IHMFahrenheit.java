7bd74161ebcdc7d9dd5571df2466237ad5ac7c4c
Hany Abboud <hany.abboud@isae.edu.lb>
2019-03-21 18:43:41 +0200

    Question 3.

--- tp2/question3/IHMFahrenheit.java
+++ tp2/question3/IHMFahrenheit.java
@@ -32,16 +32,21 @@ public class IHMFahrenheit extends JFrame implements ActionListener{
    */
   public void actionPerformed( ActionEvent ae ){
     try{
-      int fahrenheit = 0; // valeur est une String et doit être convertie en entier, voir java.lang.Integer méthode parseInt (--> try/catch)
-      float celsius = 0F; // à compléter, en appelant la méthode ad'hoc de la question2 
-      // un test ici pour le zéro absolu (-273.1)
-
+      int fahrenheit = Integer.parseInt(entree.getText()); // valeur est une String et doit être convertie en entier, voir java.lang.Integer méthode parseInt (--> try/catch)
+      
+      float celsius;
+      if(entree.getText().equals("0")) celsius = -273.1F; // un test ici pour le zéro absolu (-273.1)
+      else celsius =  fahrenheitEnCelsius(fahrenheit); // à compléter, en appelant la méthode ad'hoc de la question2 
+      
       sortie.setText( Float.toString( celsius));
     }catch(NumberFormatException nfe){
       sortie.setText("error ! ");
     }
   }
   
+  public static float fahrenheitEnCelsius(int f) {
+      return (float) (((int) (((float) ((5 * (f - 32)) / 9.0))*10))/10.0);
+  }
   
   public static void main(String[] args){
       new IHMFahrenheit();
